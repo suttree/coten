323450bc99f47546004cc3804833401721985211
Duncan Gough <duncan.gough@analogfolk.com>
2019-08-02 10:54:23 +0100

    ingest

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # coten
+
+# Using code from https://github.com/lvzon/kindle-clippings/blob/master/extract-kindle-clippings.py
+#   Open kindle quote file
+#       Shuffle list and iterate through
+#           reject duplicates, empties and too-long quotes
+#       Create an image from the quote
+# 
+# Convert images into a video
+# Output and save the video
+
+# Setup:
+pip install virtualenv
+virtualenv env --python=python3.6
+source env/bin/activate
+pip install numpy
+pip install opencv-python
+
+# Questions:
+Output to USB drive or run via a Raspberry Pi Zero?
+
+# Links:
+https://github.com/lvzon/kindle-clippings
+
+https://code-maven.com/create-images-with-python-pil-pillow
+
+https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
+
+http://tsaith.github.io/combine-images-into-a-video-with-python-3-and-opencv-3.html
+
+https://stackoverflow.com/questions/44947505/how-to-make-a-movie-out-of-images-in-python/44948030
+
+https://medium.com/@enriqueav/how-to-create-video-animations-using-python-and-opencv-881b18e41397
--- coten.py
+++ coten.py
@@ -1,32 +1,117 @@
-# Using code from https://github.com/lvzon/kindle-clippings/blob/master/extract-kindle-clippings.py
-#   Open kindle quote file
-#       Shuffle list and iterate through
-#           reject duplicates, empties and too-long quotes
-#       Create an image from the quote
-# 
-# Convert images into a video
-# Output and save the video
+import re
+import hashlib
+#from dateutil.parser import parse
+import os
+from datetime import datetime, timedelta, timezone
+import getpass
+import sys
 
-# Setup:
-pip install virtualenv
-virtualenv env --python=python3.6
-source env/bin/activate
-pip install numpy
-pip install opencv-python
 
-# Questions:
-Output to USB drive or run via a Raspberry Pi Zero?
+infile = 'My Clippings.txt'
 
-# Links:
-https://github.com/lvzon/kindle-clippings
+note_sep = '=========='
 
-https://code-maven.com/create-images-with-python-pil-pillow
+commentstr = '.. '  # RST (reStructuredText) comment
 
-https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
+regex_title = re.compile('^(.*)\((.*)\)$')
+regex_info = re.compile(r'^- (\S+) (.*)[\s|]+Added on\s+(.+)$')
+regex_loc = re.compile('Loc\. ([\d\-]+)')
+regex_page = re.compile('Page ([\d\-]+)')
+regex_date = re.compile('Added on\s+(.+)$')
 
-http://tsaith.github.io/combine-images-into-a-video-with-python-3-and-opencv-3.html
+regex_hashline = re.compile('^\.\.\s*([a-fA-F0-9]+)' + '\s*')
 
-https://stackoverflow.com/questions/44947505/how-to-make-a-movie-out-of-images-in-python/44948030
 
-https://medium.com/@enriqueav/how-to-create-video-animations-using-python-and-opencv-881b18e41397
+pub_title = {}
+pub_author = {}
+pub_notes = {}
+pub_hashes = {}
+
+notes = {}
+locations = {}
+types = {}
+dates = {}
+
+existing_hashes = {}
+
+print('Processing clippings file', infile)
+        
+mc = open(infile, 'r')
+
+mc.read(1)  # Skip first character
+
+line = mc.readline().strip()
+        
+while line:
+    key = line.strip()
+    result_title = regex_title.findall(key)    # Extract title and author
+    line = mc.readline().strip()                # Read information line
+    note_type, location, date = regex_info.findall(line)[0]    # Extract note type, location and date
+    result_loc = regex_loc.findall(location)
+    result_page = regex_page.findall(location)
+
+    if len(result_title):
+        title, author = result_title[0]
+    else:
+        title = key
+        author = 'Unknown'
+        
+    if len(result_loc):
+        note_loc = result_loc[0]
+    else:
+        note_loc = ''
+        
+    if len(result_page):
+        note_page = result_page[0]
+    else:
+        note_page = ''
+        
+    note_text = ''
+    line = mc.readline()                # Skip empty line
+    line = mc.readline().strip()
+        
+    while line != note_sep:
+        note_text += line + '\n'
+        line = mc.readline().strip()
+    
+    note_hash = hashlib.sha256(note_text.strip().encode('utf8')).hexdigest()[:8]
+    
+    if key not in pub_notes:
+        pub_notes[key] = []
+        pub_hashes[key] = []
+        
+    pub_title[key] = title.strip()
+    pub_author[key] = author.strip()
+    pub_notes[key].append(note_text.strip())
+    pub_hashes[key].append(note_hash)
+    
+    locstr = ''
+    if note_loc:
+        locstr = 'loc.' + note_loc
+    if note_page:
+        if note_loc:
+            locstr += ', '
+        locstr += 'p.' + note_page
+        
+    try:
+        datestr = str(parse(date))
+    except:
+        datestr = date
+    
+    notes[note_hash] = note_text.strip()
+    locations[note_hash] = locstr
+    types[note_hash] = note_type
+    dates[note_hash] = datestr
+        
+    line = mc.readline().strip()
+
+mc.close()
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(notes)
+pp.pprint(locations)
+pp.pprint(types)
+pp.pprint(dates)
+
 
